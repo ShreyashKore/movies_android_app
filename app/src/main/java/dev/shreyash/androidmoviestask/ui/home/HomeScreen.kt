c68621bbe60c8ca1ef3a0f9f566f9d59384c911c
Shreyash Kore <kore.shreyash@gmail.com>
2024-12-15 20:16:22 +0530

    add simple loading and error states handling logic

--- app/src/main/java/dev/shreyash/androidmoviestask/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/shreyash/androidmoviestask/ui/home/HomeScreen.kt
@@ -2,6 +2,7 @@ package dev.shreyash.androidmoviestask.ui.home
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +12,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScrollableTabRow
@@ -22,12 +24,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.shreyash.androidmoviestask.R
 import dev.shreyash.androidmoviestask.data.models.Movie
+import dev.shreyash.androidmoviestask.ui.components.ErrorMessageWithRetry
 import dev.shreyash.androidmoviestask.ui.home.components.MoviePoster
 import dev.shreyash.androidmoviestask.ui.home.components.TrendingMoviePoster
 
@@ -37,28 +41,50 @@ fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(), onMovieClick: (Int) -> Unit
 ) {
     HomeScreen(
+        isLoading = viewModel.isLoading.collectAsState().value,
+        exception = viewModel.exception.collectAsState().value,
         trendingMovies = viewModel.trendingMovies.collectAsState().value,
         nowPlayingMovies = viewModel.nowPlayingMovies.collectAsState().value,
         popularMovies = viewModel.popularMovies.collectAsState().value,
         topRatedMovies = viewModel.topRatedMovies.collectAsState().value,
         upcomingMovies = viewModel.upcomingMovies.collectAsState().value,
-        onMovieClick = onMovieClick
+        onMovieClick = onMovieClick,
+        onRetry = viewModel::fetchMovies
     )
 }
 
 
 @Composable
 fun HomeScreen(
+    isLoading: Boolean,
+    exception: Exception?,
     trendingMovies: List<Movie>,
     nowPlayingMovies: List<Movie>,
     popularMovies: List<Movie>,
     topRatedMovies: List<Movie>,
     upcomingMovies: List<Movie>,
-    onMovieClick: (id: Int) -> Unit
+    onMovieClick: (id: Int) -> Unit,
+    onRetry: () -> Unit,
 ) {
     var selectedTab by remember { mutableIntStateOf(0) }
 
     Scaffold { innerPadding ->
+        if (isLoading || exception != null) {
+            Box(Modifier.fillMaxSize().padding(innerPadding), contentAlignment = Alignment.Center) {
+                if (isLoading) {
+                    CircularProgressIndicator()
+                } else {
+                    ErrorMessageWithRetry(
+                        title = stringResource(R.string.error_occurred),
+                        message = exception?.message ?: "Unknown error",
+                        onRetry = onRetry
+                    )
+                }
+            }
+            return@Scaffold
+        }
+
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -79,7 +105,7 @@ fun HomeScreen(
                     modifier = Modifier.padding(vertical = 6.dp)
                 )
                 LazyRow {
-                    itemsIndexed(trendingMovies) { i, movie ->
+                    itemsIndexed(trendingMovies.take(5)) { i, movie ->
                         TrendingMoviePoster(i + 1, movie, onMovieClick)
                     }
                 }
--- app/src/main/java/dev/shreyash/androidmoviestask/ui/home/HomeViewModel.kt
+++ app/src/main/java/dev/shreyash/androidmoviestask/ui/home/HomeViewModel.kt
@@ -1,19 +1,31 @@
 package dev.shreyash.androidmoviestask.ui.home
 
+import android.net.http.HttpException
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.media3.common.util.Log
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.shreyash.androidmoviestask.data.models.Movie
 import dev.shreyash.androidmoviestask.domain.MoviesRepository
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val moviesRepository: MoviesRepository
 ) : ViewModel() {
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    private val _exception = MutableStateFlow<Exception?>(null)
+    val exception = _exception.asStateFlow()
+
     private val _trendingMovies = MutableStateFlow<List<Movie>>(emptyList())
     val trendingMovies = _trendingMovies.asStateFlow()
 
@@ -33,13 +45,33 @@ class HomeViewModel @Inject constructor(
         fetchMovies()
     }
 
-    private fun fetchMovies() {
-        viewModelScope.launch {
-            _trendingMovies.value = moviesRepository.getTrendingMovies("day").results.take(5)
-            _nowPlayingMovies.value = moviesRepository.getNowPlayingMovies().results
-            _popularMovies.value = moviesRepository.getPopularMovies().results
-            _topRatedMovies.value = moviesRepository.getTopRatedMovies().results
-            _upcomingMovies.value = moviesRepository.getUpcomingMovies().results
+    fun fetchMovies() {
+        viewModelScope.launch(CoroutineExceptionHandler { _, e ->
+            Log.e("HomeScreen", "$e")
+        }) {
+            try {
+                _isLoading.value = true
+                _exception.value = null
+                val trendingMoviesDeferred = async { moviesRepository.getTrendingMovies("day").results.take(5) }
+                val nowPlayingMoviesDeferred = async { moviesRepository.getNowPlayingMovies().results }
+                val popularMoviesDeferred = async { moviesRepository.getPopularMovies().results }
+                val topRatedMoviesDeferred = async { moviesRepository.getTopRatedMovies().results }
+                val upcomingMoviesDeferred = async { moviesRepository.getUpcomingMovies().results }
+                _trendingMovies.value = trendingMoviesDeferred.await()
+                _nowPlayingMovies.value = nowPlayingMoviesDeferred.await()
+                _popularMovies.value = popularMoviesDeferred.await()
+                _topRatedMovies.value = topRatedMoviesDeferred.await()
+                _upcomingMovies.value = upcomingMoviesDeferred.await()
+            } catch (e: HttpException) {
+                _exception.value = e
+            } catch (e: IOException) {
+                // can add retry mechanism here
+                _exception.value = e
+            } catch (e: Exception) {
+                _exception.value = e
+            } finally {
+                _isLoading.value = false
+            }
         }
     }
 }
